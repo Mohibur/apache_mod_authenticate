9e12b51e7bb573b2418e939394a676f7eff5d7b5
Mohibur Rashid <johny101519181@yahoo.com>
2019-12-14 23:46:39 +0900

    joy

--- mod_authenticate/mod_authenticate.c
+++ mod_authenticate/mod_authenticate.c
@@ -6,7 +6,11 @@
 /* Define prototypes of our functions in this module */
 static void register_hooks(apr_pool_t *pool);
 static int mod_session_validation(request_rec *r);
-static const char *set_single_config(cmd_parms *cmd, void *cfg, const char *arg);
+
+static const char *set_script_path(cmd_parms *cmd, void *cfg, const char *arg);
+static const char *set_session_cookie_name(cmd_parms *cmd, void *cfg, const char *arg);
+static const char *set_processor_name(cmd_parms *cmd, void *cfg, const char *arg);
+
 typedef struct {
   const char *script_path;                   /* Scirpt Path */
   const char *cookie_name;       /* target cookie name */
@@ -17,27 +21,39 @@ typedef struct {
 #define SESSION_COOKIE_NAME   "SessionCookieName"
 #define VALIDATE_PROCESSOR    "ValidateProcessor"
 
-
-
 static configuration config;
 
 /* Define our module as an entity and assign a function for registering hooks  */
 
 static const command_rec configuration_directive[] =
 {
-    AP_INIT_TAKE1(VALIDATE_SCRIPT_PATH, set_single_config, NULL, RSRC_CONF, "The path"),
-    AP_INIT_TAKE1(SESSION_COOKIE_NAME,  set_single_config, NULL, RSRC_CONF, "The path to whatever"),
-    AP_INIT_TAKE1(VALIDATE_PROCESSOR,  set_single_config, NULL, RSRC_CONF, "The path to whatever"),
+    AP_INIT_TAKE1(VALIDATE_SCRIPT_PATH, set_script_path, NULL, RSRC_CONF, "The path"),
+    AP_INIT_TAKE1(SESSION_COOKIE_NAME,  set_session_cookie_name, NULL, RSRC_CONF, "The path to whatever"),
+    AP_INIT_TAKE1(VALIDATE_PROCESSOR,   set_processor_name, NULL, RSRC_CONF, "The path to whatever"),
     { NULL }
 };
 
-const char *validate_path(cmd_parms *cmd, void *cfg, const char *arg)
-{
-  if(strcmp(arg, VALIDATE_SCRIPT_PATH) == 0) config.script_path=arg;
-  else if(strcmp(arg, SESSION_COOKIE_NAME) == 0) config.cookie_name=arg;
-  else if(strcmp(arg, VALIDATE_PROCESSOR) == 0) config.script_processor=arg;
+const char *set_script_path(cmd_parms *cmd, void *cfg, const char *arg) {
+  //void *pointer = malloc(strlen(arg)+1);
+  //strcpy(pointer, arg);
+  config.script_path=arg;
   return NULL;
 }
+const char *set_session_cookie_name(cmd_parms *cmd, void *cfg, const char *arg) {
+  //void *pointer = malloc(strlen(arg)+1);
+  //strcpy(pointer, arg);
+  config.cookie_name=arg;
+  return NULL;
+}
+
+const char *set_processor_name(cmd_parms *cmd, void *cfg, const char *arg) {
+  //void *pointer = malloc(strlen(arg)+1);
+  //strcpy(pointer, arg);
+  config.script_processor=arg;
+  return NULL;
+}
+
+
 
 module AP_MODULE_DECLARE_DATA  validating_module =
 {
@@ -54,7 +70,7 @@ module AP_MODULE_DECLARE_DATA  validating_module =
 /* register_hooks: Adds a hook to the httpd process */
 static void register_hooks(apr_pool_t *pool) {
   /* Hook the request handler */
-  ap_hook_handler(mod_session_validation, NULL, NULL, APR_HOOK_LAST);
+  ap_hook_handler(mod_session_validation, NULL, NULL, APR_HOOK_FIRST);
 }
 
 /* The handler function for our module.
@@ -66,17 +82,24 @@ static int mod_session_validation(request_rec *r) {
    * If it is, we accept it and do our things, it not, we simply return DECLINED,
    * and Apache will try somewhere else.
    */
-  if (!r->handler || strcmp(r->handler, "validation-handler")) return (DECLINED);
-  //    ap_set_content_type(r, "text/html");    
-  // The first thing we will do is write a simple "Hello, world!" back to the client.
   const char *cookie = NULL;
   ap_cookie_read(r, config.cookie_name, &cookie, 0);
-  ap_rprintf(r, "Cookie Value: %s<br>", cookie);
+ 
+#ifdef TESTINGNOW
+  if (!r->handler || strcmp(r->handler, "validation-handler")) return (DECLINED);
+  ap_set_content_type(r, "text/html");    
+  // The first thing we will do is write a simple "Hello, world!" back to the client.
+ ap_rprintf(r, "Cookie Value: %s<br>", cookie);
   ap_rprintf(r, "r->uri: %s<br>", r->uri);
   ap_rprintf(r, "script_path: %s<br>", config.script_path);
   ap_rprintf(r, "cookie_name: %s<br>", config.cookie_name);
   ap_rprintf(r, "script_processor: %s<br>", config.script_processor);
-
-  return OK;
+#else
+  if(cookie == NULL || strcmp(cookie, "") == 0) {
+    return HTTP_UNAUTHORIZED;
+  }
+#endif
+  
+  return DECLINED;
 }
 
