9f656895f766573793bce9d109e439744a2b3167
Mohibur Rashid <johny101519181@yahoo.com>
2019-12-15 11:40:53 +0900

    update

--- lua_mod/run_lua.c
+++ lua_mod/run_lua.c
@@ -5,31 +5,59 @@
 #include <stdlib.h>                             /* For function exit() */
 #include <stdio.h>                              /* For input/output */
 
-void bail(lua_State *L, char *msg){
-	fprintf(stderr, "\nFATAL ERROR:\n  %s: %s\n\n",
-		msg, lua_tostring(L, -1));
-	exit(1);
+#include <authenticate_scripts.h>
+
+script int fail_process(FILE *f, int error_no, char *script, char *key) {
+  if(f == null) return error_no;
+  char *message_string = "Process failed. Reason being: %s\n";
+  if(SCRIPT_ERROR_FILE_MISSING == error_no) {
+    // assuming the message below is no longer than 100; for now
+    char *msg = malloc(100 + strlen(script));
+    sprintf(msg, "Lua Execution Scirpt %s is missing", script);
+    fprintf(f, messsage_string, msg);
+    free(msg);
+  } else if(SCRIPT_ERROR_CONNECTION == error_no) {
+    fprintf(f, messsage_string, "Failed to connect with databse, please check your settings.");
+  } else if(SCRIPT_ERROR_BAD_COMMAND == error_no) {
+    fprintf(f, messsage_string, "Provided query failed");
+  } else if(SCRIPT_UNWRITTABLE_LOG == error_no) {
+    fprintf(f, messsage_string, "provided log file name is not writable");
+  } if(SCRIPT_ERROR_UNSPECIFIED == error_no) {
+    fprintf(f, messsage_string, "Unspecified error");
+  }
+  return error_no;
 }
 
-int main(void)
-{
-    lua_State *L;
+int exec_external_command (char *key, char *script, char *log_file) {
+  FILE *f;
 
-    L = luaL_newstate();                        /* Create Lua state variable */
-    luaL_openlibs(L);                           /* Load Lua libraries */
+  if(log_file != NULL && strcmp(log_file, "") != 0)
+    f = NULL;
+  else if (access(log_file, F_OK) != -1 ) {
+    f = fopen(log_file, "a");
+    if(f == NULL) {
+      return SCRIPT_UNWRITTABLE_LOG;
+    }
+  }
+  if (access(script, F_OK) == -1) {
+    return fail_process(f, SCRIPT_ERROR_FILE_MISSING, script, key);
+  }
 
-    if (luaL_loadfile(L, "helloscript.lua"))    /* Load but don't run the Lua script */
-	bail(L, "luaL_loadfile() failed");      /* Error out if file can't be read */
+  lua_State *L;
+  
+  L = luaL_newstate();                        /* Create Lua state variable */
+  luaL_openlibs(L);                           /* Load Lua libraries */
 
-    printf("In C, calling Lua\n");
+  if (luaL_loadfile(L, target_script))    /* Load but don't run the Lua script */
+    fail_process(f--#TODO);      /* Error out if file can't be read */
 
-    if (lua_pcall(L, 0, 0, 0))                  /* Run the loaded Lua script */
-	bail(L, "lua_pcall() failed");          /* Error out if Lua file has an error */
+  printf(f, "In C, calling Lua\n");
 
-    printf("Back in C again\n");
-
-    lua_close(L);                               /* Clean up, free the Lua state var */
-
-    return 0;
+  if (lua_pcall(L, 0, 0, 0)) {                  /* Run the loaded Lua script */
+    bail(f, L, "lua_pcall() failed");          /* Error out if Lua file has an error */
+    return 
+  }
+  lua_close(L);                               /* Clean up, free the Lua state var */
+  return 0;
 }
 
---
+++ mod_authenticate/authenticate_scripts.h
@@ -0,0 +1,21 @@
+#ifdef __scripts_h__
+#define __scripts_h__
+
+
+// ERROR messages
+#define SCRIPT_SUCCESS_VALIDATE                 0x000
+#define SCRIPT_FAILED_VALIDATE                  0x001
+#define SCRIPT_ERROR_FILE_MISSING               0x002
+#define SCRIPT_ERROR_CONNECTION                 0x003
+#define SCRIPT_ERROR_BAD_COMMAND                0x004
+#define SCRIPT_UNWRITTABLE_LOG                  0x005
+#define SCRIPT_ERROR_UNSPECIFIED                0x500
+
+// COMMON FUNCTIONS
+/*
+  * key: if empty, will return failed: Expected to be the key that needed to be retrieved from your source
+  * script: target script absolute path. If not valid path or file does not exists, apache will failed to start
+  * log: if not properly written 
+  */
+int exec_validate_cmd (char *key, char *script, char *log);
+#endif
